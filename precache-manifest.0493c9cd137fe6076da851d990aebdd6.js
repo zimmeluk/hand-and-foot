ccb6e79d4b951f3bfad827d77bc4bf82a1460b1c
Luke Zimmerman <zimmeluk14@gmail.com>
2020-07-11 16:46:36 -0500

    Updates

---
+++ precache-manifest.0493c9cd137fe6076da851d990aebdd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06b76a0aff389a9e1cc6b50737b027f0",
+    "url": "/hand-and-foot/index.html"
+  },
+  {
+    "revision": "534d1225652e5c0880e4",
+    "url": "/hand-and-foot/static/css/main.4177cd34.chunk.css"
+  },
+  {
+    "revision": "b8a05ba39b39f22aa13a",
+    "url": "/hand-and-foot/static/js/2.e6d94bea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hand-and-foot/static/js/2.e6d94bea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "534d1225652e5c0880e4",
+    "url": "/hand-and-foot/static/js/main.709eb32f.chunk.js"
+  },
+  {
+    "revision": "d7b0ff8d45bd150ee9ac",
+    "url": "/hand-and-foot/static/js/runtime-main.2503df06.js"
+  }
+]);
